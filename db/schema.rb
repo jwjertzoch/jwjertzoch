cd98ea2b090f4905bad84d44e7324b0457996ef3
jwjertzoch <jenny_wjertzoch@yahoo.de>
2018-02-14 20:33:15 +0100

    add_price_to_products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180209101002) do
+ActiveRecord::Schema.define(version: 20180214100930) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180209101002) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "color"
+    t.string "price"
   end
 
 end
