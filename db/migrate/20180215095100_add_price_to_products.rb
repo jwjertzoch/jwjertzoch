08215abaa546285a6304b61263f1c52c52c8e766
jwjertzoch <jenny_wjertzoch@yahoo.de>
2018-02-15 14:28:32 +0100

    duplicate_column

--- db/migrate/20180215095100_add_price_to_products.rb
+++ db/migrate/20180215095100_add_price_to_products.rb
@@ -3,3 +3,8 @@ class AddPriceToProducts < ActiveRecord::Migration[5.1]
     add_column :products, :price, :string
   end
 end
+
+if column_exists? :products, :price
+               remove_column :products, :price
+       end
+     end
