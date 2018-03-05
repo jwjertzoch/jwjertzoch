a518dbf19f9888167531d478e6af5e35649c586b
jwjertzoch <jenny_wjertzoch@yahoo.de>
2018-03-05 21:14:17 +0100

    5.4: Authentication

--- db/migrate/20180215211244_add_price_to_products.rb
+++ db/migrate/20180215211244_add_price_to_products.rb
@@ -1,5 +1,5 @@
 class AddPriceToProducts < ActiveRecord::Migration[5.1]
   def change
-    add_column :products, :price, :strings, default: 0
+    add_column :products, :price, :string, default: 0
   end
 end
