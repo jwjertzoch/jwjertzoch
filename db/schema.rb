109a43aca7ac6f77a24aec24bc8026a2433a1026
jwjertzoch <jenny_wjertzoch@yahoo.de>
2018-03-16 12:53:02 +0100

    5.10: Admin Role & Moderating User Comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309103911) do
+ActiveRecord::Schema.define(version: 20180316112315) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20180309103911) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
