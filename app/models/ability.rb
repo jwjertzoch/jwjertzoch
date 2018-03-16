d68995044e44ddb39aac459b8049394f3f15b798
jwjertzoch <jenny_wjertzoch@yahoo.de>
2018-03-16 13:01:45 +0100

    5.10: Admin Role & Moderating User Comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,15 +1,12 @@
-
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-    can :read, :all . # permissions for every user, even if not logged in
-    if user.present?  # additional permissions for logged in users (they can manage their posts)
-      can :manage, Post, user_id: user.id
-      if user.admin?  # additional permissions for administrators
-        can :manage, :all
-      end
+    user ||= User.new #guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+    else
+      can :manage, User, id: user.id
     end
   end
 end
