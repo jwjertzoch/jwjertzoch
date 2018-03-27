9bc46fa57e0f5a32ff98d327b515aa13e52bd088
jwjertzoch <jenny_wjertzoch@yahoo.de>
2018-03-27 11:34:55 +0200

    Product Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,14 @@ class Product < ApplicationRecord
   has_many :comments
 
   validates :name, presence: true
+  validates :description, length: { minimum: 20 }
+  validates :category, presence: true
+  validates :image_url, format: {with: /\.(png|jpg|jpeg)\Z/i}
+  validates :price, numericality: {greater_than_or_equal_to: 0}
+  # define scope for fetching products that have images
+  scope :products_with_images, -> {where.not(image_url: nil)}
+  # merge scope that is defined in comment model 'recent_comments scope'
+  scope :review_recent_comments, -> {joins(:comments).merge(Comment.recent_comments)}
 
   def self.search(search_term)
     Product.where("name LIKE ?", "%#{search_term}%")
