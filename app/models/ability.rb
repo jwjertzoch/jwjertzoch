9b05ef85104d58b8386689081c01bc75b4fd5d89
jwjertzoch <jenny_wjertzoch@yahoo.de>
2018-04-22 10:52:23 +0200

    5.11: Consumer Web App

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,11 +2,12 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new #guest user (not logged in)
-    if user.admin?
-      can :manage, :all
-    else
-      can :manage, User, id: user.id
+    user ||= User.new # guest user (not logged in)
+    can :manage, User, id: user.id
+    can :manage, Product
+    cannot :destroy, Comment
+    if user.admin?  # additional permissions for administrators
+        can :manage, :all
     end
   end
 end
