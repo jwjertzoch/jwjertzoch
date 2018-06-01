3d2cc848c901292c79fd89e48656076de8326487
jwjertzoch <jenny_wjertzoch@yahoo.de>
2018-06-01 12:11:51 +0200

    6.11: Caching

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
+  # Memecachier - Dalli
+  config.cache_store = :dalli_store,
+  (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+  {:username => ENV["MEMCACHIER_USERNAME"],
+    :password => ENV["MEMCACHIER_PASSWORD"],
+    :failover => true,
+    :socket_timeout => 1.5,
+    :socket_failure_delay => 0.2,
+    :down_retry_delay => 60
+  }
+
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
