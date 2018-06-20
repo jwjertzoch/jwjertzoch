0a554f8153b963979bd9335d9d7c027a0fd41f6f
jwjertzoch <jenny_wjertzoch@yahoo.de>
2018-06-20 21:12:37 +0200

    action_cable_disable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,6 +104,6 @@ Rails.application.configure do
 
   config.web_socket_server_url = "wss://capeline.herokuapp.com/cable"
   config.action_cable.allowed_request_origins = ['https://capeline.herokuapp.com', 'http://capeline.herokuapp.com']
+  config.action_cable.disable_request_forgery_protection = true;
 
-  
 end
