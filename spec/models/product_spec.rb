eb1ef91dd213e1904f4191985ae540169f34bee7
jwjertzoch <jenny_wjertzoch@yahoo.de>
2018-06-24 21:23:06 +0200

    6.15: More Tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,8 +5,7 @@ describe Product do
   context "when the product has comments" do
 
     let(:product) { Product.create!(name: "Fawn", description: "This handmade 100% cotton baby bonnet is for babies first months. Take care: Bonnets should hand-wash cold, dry flat. Iron if necessary. Do not bleach or use oxidizing agents. ", image_url: "/home/jenny/myapp/app/assets/images/BraunBonnet2web.jpg", colour: "Brown", price: "19.95") }
-
-    let(:user) { User.create!(email: "jenny_wjertzoch@yahoo.de",first_name: "Jennifer", last_name: "Wjertzoch", password: "M4l1bu!") }
+    let(:user) { User.create!(email: "jenny_wjertzoch@yahoo.de",first_name: "Jennifer", last_name: "Wjertzoch", password: "Test123") }
 
     before do
       product.comments.create!(rating: 1, user: user, body: "Awful!")
@@ -20,7 +19,22 @@ describe Product do
   end
 
     it "is not valid without a name" do
+      expect(Product.new(name: "test")).not_to be_valid
+    end
+
+    it "is not valid without a discription" do
       expect(Product.new(description: "test")).not_to be_valid
     end
-    
+
+    it "is not valid without a price" do
+      expect(Product.new(price: "19.95")).not_to be_valid
+    end
+
+    it "is not valid without a colour" do
+      expect(Product.new(colour: "brown")).not_to be_valid
+    end
+
+    it "is not valid without a image_url" do
+      expect(Product.new(image_url: "bonnet.jpg")).not_to be_valid
+    end
 end
